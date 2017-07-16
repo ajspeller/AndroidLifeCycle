18f352c0f87f7ce2a3533c7ba7904e5c02f1c250
AJ Speller <ajspeller@gmail.com>
2017-07-15 20:38:40 -0400

    Creation of the overriddent functions that log when they are called.

--- app/src/main/java/com/ajspeller/androidlifecycle/MainActivity.java
+++ app/src/main/java/com/ajspeller/androidlifecycle/MainActivity.java
@@ -1,13 +1,53 @@
 package com.ajspeller.androidlifecycle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    String tag = "Lifecycle Step";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(tag, " In the onCreate() event");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(tag, "In the onStart() event");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(tag, "In the onRestart() event");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(tag, "In the onResume() event");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(tag, "In the onPause() event");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(tag, "In the onStop() event");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(tag, "In the onDestroy() event");
     }
 }
